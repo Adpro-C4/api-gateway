6988b03a9ad7eac68bd8ba5a67fe6cc6697bf3d2
isaui <isadestroyed17@gmail.com>
2024-05-07 10:17:53 +0700

    feat: setting gateway for product, user-data and auth

--- src/main/java/com/gateway/apigateway/GatewayConfig.java
+++ src/main/java/com/gateway/apigateway/GatewayConfig.java
@@ -9,20 +9,21 @@ import org.springframework.context.annotation.Configuration;
 public class GatewayConfig {
 
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        System.out.println("P");
+    RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("purchase-service", r -> r.path("/purchase/**")
+                .route("purchase-service-id", r -> r.path("/purchase-service/**")
+                        .filters(f -> f.rewritePath("/purchase-service/(?<remaining>.*)", "/$\\{remaining}")) 
                         .uri("https://purchase-service-specialitystore.up.railway.app"))
-                .route("auth-service", r -> r.path("/auth/**")
-                        .uri("https://specialitystorebackend.up.railway.app"))
-                .route("user-data-service", r -> r.path("/user/data/**")
-                        .uri("https://specialitystorebackend.up.railway.app/data/"))
-                .route("product-data-service", r -> r.path("/product/all")
-                        .uri("https://microservice-katalog-production.up.railway.app/"))
+                .route("auth-service", r -> r.path("/auth-service/**")
+                        .filters(f -> f.rewritePath("/auth-service/(?<remaining>.*)", "/$\\{remaining}")) 
+                        .uri("https://specialitystorebackend.up.railway.app/auth"))
+                .route("user-data-service", r -> r.path("/user-data-service/**") // Menyesuaikan path ke /user-data-service/**
+                        .filters(f -> f.rewritePath("/user-data-service/(?<remaining>.*)", "/$\\{remaining}")) // Rewrite path agar sesuai dengan prefix
+                        .uri("https://specialitystorebackend.up.railway.app/data"))
+                .route("product-data-service", r -> r.path("/product-service/**")
+                .filters(f -> f.rewritePath("/product-service/(?<remaining>.*)", "/$\\{remaining}")) 
+                        .uri("https://microservice-katalog-production.up.railway.app"))
                 .build();
     }
-    // https://specialitystorebackend.up.railway.app/data/customer/4
-    // http://localhost:8080/user/data/customer/4
 }
 
